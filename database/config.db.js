274d829892c5e72292ec52b16ddd17c75b2d5682
SebasDev807 <156554131+SebasDev807@users.noreply.github.com>
2024-06-20 13:58:45 -0500

    Solucionando problema del timeout

--- database/config.db.js
+++ database/config.db.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 
     try {
-        await mongoose.connect(process.env.MONGO_CNN);
+        await mongoose.connect(process.env.MONGO_CNN, {
+            useNewUrlParser: true,
+            serverSelectionTimeoutMS: 30000, // 30 segundos
+            socketTimeoutMS: 45000, // 45 segundos
+            connectTimeoutMS: 30000, // 30 segundos
+        });
         console.log('Conexión exitosa :D');
-        
+
     } catch (error) {
         console.log(error);
         throw new Error('Database Error', error.message);
